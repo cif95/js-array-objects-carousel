835b90b7cf9c0ae9cb0b56f24895a68e7f6f33ba
cif95 <vivianacifarelli9@gmail.com>
2022-03-10 16:44:06 +0100

    implemented array of objects and bonus 1

--- js/main.js
+++ js/main.js
@@ -16,3 +16,151 @@ E se volessi un bottone per invertire la "direzione" del carosello?
 
  *
  */
+
+const carousel = [
+	{
+		image: "https://picsum.photos/775/540?random=1",
+		title: 'Lorem Ipsum',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis est fugiat animi veniam, corrupti quaerat velit temporibus dolore soluta, corporis ullam magni',
+	},
+	{
+		image: "https://picsum.photos/775/540?random=2",
+		title: 'Lorem Ipsum',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis est fugiat animi veniam, corrupti quaerat velit temporibus dolore soluta, corporis ullam magni',
+	},
+	{
+		image: "https://picsum.photos/775/540?random=3",
+		title: 'Lorem Ipsum',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis est fugiat animi veniam, corrupti quaerat velit temporibus dolore soluta, corporis ullam magni',
+	},
+	{
+		image: "https://picsum.photos/775/540?random=4",
+		title: 'Lorem Ipsum',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis est fugiat animi veniam, corrupti quaerat velit temporibus dolore soluta, corporis ullam magni',
+	},
+	{
+		image: "https://picsum.photos/775/540?random=5",
+		title: 'Lorem Ipsum',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis est fugiat animi veniam, corrupti quaerat velit temporibus dolore soluta, corporis ullam magni',
+	}
+];
+
+
+
+for (let i = 0 ; i < carousel.length ; i++){
+   document.querySelector('div.my-carousel-images').innerHTML += 
+	`<figure class="carousel-current-item position-relative">
+		<img src="${carousel[i].image}" alt="${carousel[i].title}">
+		<figcaption id="carusel-info" class="position-absolute bottom-0 end-0 text-white text-end p-5">
+		<h2>${carousel[i].title}</h2>
+		<p>${carousel[i].text}</p>
+		</figcaption>
+	</figure>`
+	document.querySelector('.my-thumbnails').innerHTML += 
+	`<img class="carousel-thumbnails-item my-filter float-left" src="${carousel[i].image}" alt="${carousel[i].title}">`
+}
+
+document.querySelector('.my-carousel-container').innerHTML += `<button class="reverse-slider btn btn-primary w-25 mx-auto rounded-3 mt-5">Reverse Slider Direction</button>`;
+
+let activeItem = 0;
+
+const carouselElements = document.getElementsByClassName('carousel-current-item');
+carouselElements[activeItem].classList.add('active');
+
+const carouselThumbnailsElements = document.getElementsByClassName('carousel-thumbnails-item');
+carouselThumbnailsElements[activeItem].classList.remove('my-filter');
+carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
+
+
+let sliderLoop;
+
+setTimeout(function() {
+	sliderLoop = setInterval ( function(){
+	carouselElements[activeItem].classList.remove('active');
+	carouselThumbnailsElements[activeItem].classList.add('my-filter');
+	carouselThumbnailsElements[activeItem].classList.remove('border', 'border-2');
+	if ( activeItem === carousel.length - 1 ){
+		activeItem = 0;
+	} else {
+		activeItem++;
+	}
+	carouselElements[activeItem].classList.add('active');
+	carouselThumbnailsElements[activeItem].classList.remove('my-filter');
+	carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
+	},1000)
+}, 4000 );
+
+
+document.querySelector('.my-next-hook').addEventListener ( 'click', function(){
+	carouselElements[activeItem].classList.remove('active');
+	carouselThumbnailsElements[activeItem].classList.add('my-filter');
+	carouselThumbnailsElements[activeItem].classList.remove('border', 'border-2');
+	if ( activeItem === carousel.length - 1 ){
+		activeItem = 0;
+	} else {
+		activeItem++;
+	}
+	carouselElements[activeItem].classList.add('active');
+	carouselThumbnailsElements[activeItem].classList.remove('my-filter');
+	carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
+} );
+
+
+document.querySelector('.my-prev-hook').addEventListener ( 'click', function() {
+	carouselElements[activeItem].classList.remove('active');
+	carouselThumbnailsElements[activeItem].classList.remove('border', 'border-2');
+	if ( activeItem === 0 ){
+		activeItem = carousel.length - 1;
+	} else {
+		activeItem--;
+	}
+	carouselThumbnailsElements[activeItem].classList.add('my-filter');
+	carouselElements[activeItem].classList.add('active');
+	carouselThumbnailsElements[activeItem].classList.remove('my-filter');
+	carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
+})
+
+// document.querySelector('button.reverse-slider').addEventListener ( 'click', function(){
+// 	clearInterval(sliderLoop);
+// 	setTimeout(function() {
+// 		sliderLoop = setInterval ( function(){
+// 			carouselElements[activeItem].classList.remove('active');
+// 		carouselThumbnailsElements[activeItem].classList.add('my-filter');
+// 		carouselThumbnailsElements[activeItem].classList.remove('border', 'border-2');
+// 		if ( activeItem === carousel.length - 1 ){
+// 			activeItem = 0;
+// 		} else {
+// 			activeItem--;
+// 		}
+// 		carouselElements[activeItem].classList.add('active');
+// 		carouselThumbnailsElements[activeItem].classList.remove('my-filter');
+// 		carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
+// 		},1000)
+// 	}, 1000 );
+// })
+
+
+
+// function addClassToElementsWithAnIndex(domElementGroup, indexOfElements, classToAdd){
+// 	domElementGroup[indexOfElements].classList.add(classToAdd);
+// }
+
+// function removeClassToElementsWithAnIndex(domElementGroup, indexOfElements, classToAdd){
+// 	domElementGroup[indexOfElements].classList.remove('classToAdd');
+// }
+
+// function decrementCarouselItemIndex(itemIndex, itemList) {
+// 	if ( itemIndex === 0 ){
+// 		itemIndex = itemList.length - 1;
+// 	} else {
+// 		itemIndex--;
+// 	}
+// }
+
+// function incrementCarouselItemIndex(itemIndex, itemList) {
+// 	if ( itemIndex === 0 ){
+// 		itemIndex = itemList.length - 1;
+// 	} else {
+// 		itemIndex++;
+// 	}
+// }
